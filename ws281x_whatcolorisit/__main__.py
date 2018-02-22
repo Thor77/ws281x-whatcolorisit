5b09bdc6151fa8361b0fe7978b8fe251d5335ea2
Thor77 <thor77@thor77.org>
2018-02-22 21:23:06 +0100

    Distribute new color slowly across all pixels
    can be disabled by setting DISTRIBUTE to False in ws281x_whatcolorisit/__main__.py

--- ws281x_whatcolorisit/__main__.py
+++ ws281x_whatcolorisit/__main__.py
@@ -4,18 +4,32 @@ from time import sleep
 from ws281x_whatcolorisit.strip import initialize_strip
 from ws281x_whatcolorisit.whatcolorisit import current_color
 
+DISTRIBUTE = True
+
+
+def distribute_color(strip, r, g, b, sleep_time=0.01):
+    for i in range(strip.numPixels()):
+        strip.setPixelColorRGB(i, r, g, b)
+        strip.show()
+        sleep(sleep_time)
+
 
 def main():
     # initialize strip
     strip = initialize_strip()
     while True:
-        # set strip to current color
+        # determine current color
         color = current_color(datetime.now().time())
-        for i in range(strip.numPixels()):
-            strip.setPixelColorRGB(
-                i, int(color.r),
-                int(color.g), int(color.b)
-            )
+        if DISTRIBUTE:
+            # distribute color slowly across all pixels
+            distribute_color(strip, int(color.r), int(color.g), int(color.b))
+        else:
+            # set color on all pixels
+            for i in range(strip.numPixels()):
+                strip.setPixelColorRGB(
+                    i, int(color.r),
+                    int(color.g), int(color.b)
+                )
         strip.show()
         sleep(0.5)
 
